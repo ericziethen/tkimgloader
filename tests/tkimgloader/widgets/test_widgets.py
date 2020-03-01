ba3db468bcee5f5e35a65631eacffb3f0cfcb186
eric <eric@home.com>
2020-03-01 14:29:47 +1100

    Initialize widget classes

--- tests/tkimgloader/widgets/test_widgets.py
+++ tests/tkimgloader/widgets/test_widgets.py
@@ -1,15 +1,20 @@
 
 import pytest
 
-from tkimgloader.widgets import Widget, WidgetCategory, WidgetType
+from tkimgloader.widgets import (
+    ButtonType, CanvasText, CanvasImageButton, Widget,
+    WidgetCategory, WidgetType
+)
 
-
-def test_init():
+###############################
+#### GENERIC WIDGET TESTS #####
+###############################
+def test_init_widget():
     widget = Widget(widget_id='id', widget_category=WidgetCategory.CANVAS, widget_type=WidgetType.TEXT, pos_x=100, pos_y=200)
 
     assert widget.id == 'id'
-    assert widget.category == WidgetCategory.CANVAS
-    assert widget.type == WidgetType.TEXT
+    assert widget.widget_category == WidgetCategory.CANVAS
+    assert widget.widget_type == WidgetType.TEXT
     assert widget.pos_x == 100
     assert widget.pos_y == 200
 
@@ -22,3 +27,35 @@ def test_invalid_category():
 def test_invalid_widget_type():
     with pytest.raises(ValueError):
         Widget(widget_id='id', widget_category=WidgetCategory.CANVAS, widget_type=None, pos_x=100, pos_y=200)
+
+
+############################
+#### CANVAS TEXT TESTS #####
+############################
+def test_init_canvas_text_widgit():
+    text = CanvasText(text_id='id', pos_x=100, pos_y=200)
+
+    assert text.id == 'id'
+    assert text.widget_category == WidgetCategory.CANVAS
+    assert text.widget_type == WidgetType.TEXT
+    assert text.pos_x == 100
+    assert text.pos_y == 200
+
+
+####################################
+#### CANVAS IMAGE BUTTON TESTS #####
+####################################
+def test_init_canvas_image_button():
+    button = CanvasImageButton(button_id='id', button_type=ButtonType.RELEASE, pos_x=100, pos_y=200)
+
+    assert button.id == 'id'
+    assert button.button_type == ButtonType.RELEASE
+    assert button.widget_category == WidgetCategory.CANVAS
+    assert button.widget_type == WidgetType.BUTTON
+    assert button.pos_x == 100
+    assert button.pos_y == 200
+
+
+def test_canvas_image_button_invalid_button_type():
+    with pytest.raises(ValueError):
+        CanvasImageButton(button_id='id', button_type=None, pos_x=100, pos_y=200)
--- tkimgloader/widgets.py
+++ tkimgloader/widgets.py
@@ -12,29 +12,65 @@ class WidgetCategory(enum.Enum):
 @enum.unique
 class WidgetType(enum.Enum):
     # pylint: disable=invalid-name
+    BUTTON = 'Button'
     TEXT = 'Text'
+
+
+@enum.unique
+class ButtonType(enum.Enum):
+    # pylint: disable=invalid-name
+    RELEASE = 'Release'
+    SWITCH = 'Switch'
 
 
 class Widget():
     def __init__(self, *, widget_id, widget_category, widget_type, pos_x, pos_y):
-        # Check Input
-        if not isinstance(widget_category, WidgetCategory):
-            raise ValueError(F'Invalid category "{widget_category}" Passed, not of type WidgetCategory')
-        # Check Input
-        if not isinstance(widget_type, WidgetType):
-            raise ValueError(F'Invalid category "{widget_type}" Passed, not of type WidgetCategory')
-
         # Set Attributes
         self.id = widget_id
-        self.category = widget_category
-        self.type = widget_type
+        self.widget_category = widget_category
+        self.widget_type = widget_type
         self.pos_x = pos_x
         self.pos_y = pos_y
 
+    @property
+    def widget_category(self):
+        return self._widget_category
 
-class Text(Widget):
-    pass
+    @widget_category.setter
+    def widget_category(self, widget_category):
+        if not isinstance(widget_category, WidgetCategory):
+            raise ValueError(F'Invalid category "{widget_category}" Passed, not of type WidgetCategory')
+        self._widget_category = widget_category
+
+    @property
+    def widget_type(self):
+        return self._widget_type
+
+    @widget_type.setter
+    def widget_type(self, widget_type):
+        if not isinstance(widget_type, WidgetType):
+            raise ValueError(F'Invalid type " {widget_type}" Passed, not of type WidgetType')
+        self._widget_type = widget_type
 
 
+class CanvasText(Widget):
+    def __init__(self, *, text_id, pos_x, pos_y):
+        super().__init__(widget_id=text_id, pos_x=pos_x, pos_y=pos_y,
+                         widget_category=WidgetCategory.CANVAS, widget_type=WidgetType.TEXT)
 
 
+class CanvasImageButton(Widget):
+    def __init__(self, *, button_id, button_type, pos_x, pos_y):
+        super().__init__(widget_id=button_id, pos_x=pos_x, pos_y=pos_y,
+                         widget_category=WidgetCategory.CANVAS, widget_type=WidgetType.BUTTON)
+        self.button_type = button_type
+
+    @property
+    def button_type(self):
+        return self._button_type
+
+    @button_type.setter
+    def button_type(self, button_type):
+        if not isinstance(button_type, ButtonType):
+            raise ValueError(F'Invalid type " {button_type}" Passed, not of type ButtonType')
+        self._button_type = button_type
